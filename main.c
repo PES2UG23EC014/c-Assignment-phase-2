0b0af7fcdea0c9366296aca84de6821062b4b820
PES2UG23EC014 <165558252+PES2UG23EC014@users.noreply.github.com>
2024-04-24 22:30:15 +0530

    Create main.c

---
+++ main.c
@@ -0,0 +1,24 @@
+/*main.c: The Main File*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "student.h"
+
+int main()
+{
+    int num_students;
+    printf("Enter the number of students: ");
+    scanf("%d", &num_students);
+
+    struct Student *students = (struct Student *)malloc(num_students * sizeof(struct Student));
+
+    for (int i = 0; i < num_students; i++) 
+    {
+        printf("\nStudent %d:\n", i + 1);
+        inputStudentData(&students[i]);
+        generateGradeCard(&students[i]);
+    }
+
+    free(students);
+    return 0;
+}
